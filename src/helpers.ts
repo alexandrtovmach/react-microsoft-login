daa364e0107a0b094dfe6ea0135c9b5202d7490f
alexandrtovmach <alexandrtovmach@gmail.com>
2020-11-22 12:32:10 +0200

    refactor: Updated component with hooks; Splitted functionality

---
+++ src/helpers.ts
@@ -0,0 +1,59 @@
+import { UserAgentApplication } from "msal";
+
+const CLIENT_ID_REGEX = /[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}/;
+
+export const getUserAgentApp = ({
+  clientId,
+  tenantUrl,
+  redirectUri,
+}: {
+  clientId: string;
+  tenantUrl?: string;
+  redirectUri?: string;
+}) => {
+  if (clientId && CLIENT_ID_REGEX.test(clientId)) {
+    return new UserAgentApplication({
+      auth: {
+        ...(redirectUri && { redirectUri }),
+        ...(tenantUrl && { authority: tenantUrl }),
+        clientId,
+        validateAuthority: true,
+        navigateToLoginRequestUrl: false,
+      },
+    });
+  }
+};
+
+export const getScopes = (graphScopes?: string[]) => {
+  const scopes = graphScopes || [];
+  if (!scopes.find((el: string) => el.toLowerCase() === "user.read")) {
+    scopes.push("user.read");
+  }
+  return scopes;
+};
+
+export const getLogger = (isDebugMode?: boolean) => (
+  name: string,
+  content?: any,
+  isError?: boolean
+) => {
+  if (isDebugMode) {
+    const style = `background-color: ${
+      isError ? "#990000" : "#009900"
+    }; color: #ffffff; font-weight: 700; padding: 2px`;
+    console.groupCollapsed("MSLogin debug");
+    console.log(`%c${name}`, style);
+    content && console.log(content.message || content);
+    console.groupEnd();
+  }
+};
+
+export const checkToIE = (): boolean => {
+  const ua = window.navigator.userAgent;
+  const msie = ua.indexOf("MSIE ");
+  const msie11 = ua.indexOf("Trident/");
+  const msedge = ua.indexOf("Edge/");
+  const isIE = msie > 0 || msie11 > 0;
+  const isEdge = msedge > 0;
+  return isIE || isEdge;
+};
